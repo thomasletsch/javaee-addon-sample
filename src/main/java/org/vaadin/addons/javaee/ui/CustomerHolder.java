1c2fc660785a7b59a2db67f604a73f2acdafefc1
Thomas Letsch <contact@thomas-letsch.de>
2012-05-04 10:57:41 +0200

    Adjusted to changes in EntityForm

--- src/main/java/org/vaadin/addons/javaee/ui/CustomerHolder.java
+++ src/main/java/org/vaadin/addons/javaee/ui/CustomerHolder.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2012 Thomas Letsch
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- *******************************************************************************/
 package org.vaadin.addons.javaee.ui;
 
 import java.io.Serializable;
@@ -22,9 +7,9 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.vaadin.addons.javaee.domain.Customer;
+import org.vaadin.addons.javaee.jpa.EntityItem;
 
 import com.vaadin.data.Container.Filter;
-import com.vaadin.data.Item;
 import com.vaadin.data.util.filter.Compare.Equal;
 
 @SessionScoped
@@ -50,7 +35,7 @@ public class CustomerHolder implements Serializable {
         return customer;
     }
 
-    public Item getItem() {
+    public EntityItem<Customer> getItem() {
         return customerContainer.getItem(customer.getId());
     }
 
