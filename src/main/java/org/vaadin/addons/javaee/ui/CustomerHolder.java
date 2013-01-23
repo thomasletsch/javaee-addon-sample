8acc35fc6920bb62df958d152f87aebe6f549892
Thomas Letsch <contact@thomas-letsch.de>
2013-01-23 17:09:40 +0100

    Improved properties

--- src/main/java/org/vaadin/addons/javaee/ui/CustomerHolder.java
+++ src/main/java/org/vaadin/addons/javaee/ui/CustomerHolder.java
@@ -6,8 +6,11 @@ import javax.enterprise.context.SessionScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import org.vaadin.addons.javaee.TranslationKeys;
 import org.vaadin.addons.javaee.domain.Customer;
+import org.vaadin.addons.javaee.i18n.TranslationService;
 import org.vaadin.addons.javaee.jpa.EntityItem;
+import org.vaadin.addons.javaee.page.Header;
 
 import com.vaadin.data.Container.Filter;
 import com.vaadin.data.util.filter.Compare.Equal;
@@ -22,15 +25,23 @@ public class CustomerHolder implements Serializable {
     @Inject
     private CustomerContainer customerContainer;
 
+    @Inject
+    private TranslationService translationService;
+
+    @Inject
+    private Header header;
+
     public CustomerHolder() {
     }
 
     public void setCustomer(@Observes Customer customer) {
         this.customer = customer;
+        updateHeader(customer);
     }
 
     public void handleCustomerChanged(@Observes CustomerChangedEvent event) {
         customer = event.getCustomer();
+        updateHeader(customer);
     }
 
     public Customer getCustomer() {
@@ -44,6 +55,10 @@ public class CustomerHolder implements Serializable {
     public Filter getFilter() {
         Filter filter = new Equal("customer", customer);
         return filter;
+    }
+
+    protected void updateHeader(Customer customer) {
+        header.setCaption(translationService.getText(TranslationKeys.TITLE_PORTAL) + ":" + customer.getFullName());
     }
 
 }
